758f53ab7649ce3472081d0518093620bbed24a9
Ethan Olsen <131eolsen@gmail.com>
2023-07-25 23:16:38 -0700

    chore: add font styles

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -3,7 +3,45 @@ const config = {
 	content: ["./src/**/*.{html,js,svelte,ts}"],
 
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["Inter", "sans-serif"],
+			},
+		},
+	},
+
+	daisyui: {
+		themes: [
+			"light",
+			"dark",
+			"cupcake",
+			"bumblebee",
+			"emerald",
+			"corporate",
+			"synthwave",
+			"retro",
+			"cyberpunk",
+			"valentine",
+			"halloween",
+			"garden",
+			"forest",
+			"aqua",
+			"lofi",
+			"pastel",
+			"fantasy",
+			"wireframe",
+			"black",
+			"luxury",
+			"dracula",
+			"cmyk",
+			"autumn",
+			"business",
+			"acid",
+			"lemonade",
+			"night",
+			"coffee",
+			"winter",
+		],
 	},
 
 	plugins: [require("daisyui")],
