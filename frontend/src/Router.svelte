163fe6dc0044b1466c78e61166dff2b29a639953
Ethan Olsen <131eolsen@gmail.com>
2023-07-27 22:06:38 -0700

    chore: min enhance screen and routing

---
+++ frontend/src/Router.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { route } from "./router";
+	import Bounce from "./screens/Bounce.svelte";
+	import Home from "./screens/Home.svelte";
+	import NotFound from "./screens/NotFound.svelte";
+</script>
+
+{#if $route.name === "Home"}
+	<Home />
+{:else if $route.name === "Bounce"}
+	<Bounce />
+{:else}
+	<NotFound />
+{/if}
---
+++ frontend/src/router.ts
@@ -0,0 +1,19 @@
+import { writable } from "svelte/store";
+
+/* NOTE: issue with access the file system with import.meta.glob to generate Screens list dynamically
+ * you must also add your screen route in the Router ifelse statement and add the screen to the Screens array
+ * if this can be fixed that'd be awesome, but if it works it works
+ */
+export const Screens = ["Home", "About", "NotFound"];
+
+export const route = writable<{ name: (typeof Screens)[number]; data?: any }>({
+	name: "Home",
+});
+
+export const goto = (name: (typeof Screens)[number], data?: any) =>
+	route.update(() => {
+		if (Screens.includes(name)) {
+			return { name, data };
+		}
+		return { name: "", data: undefined };
+	});
